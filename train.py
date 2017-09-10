7b239ee236ed08285eba15a1a43ce47624f61ecc
peisuke <peisuke.com@gmail.com>
2017-09-11 00:00:00 +0900

    initial commit

---
+++ train.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+import argparse
+
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import training
+from chainer.training import extensions
+
+from chainer.datasets import get_cifar10
+from chainer.datasets import get_cifar100
+
+class Net(chainer.Chain):
+    def __init__(self, n_out):
+        super(Net, self).__init__()
+        with self.init_scope():
+            self.c1 = L.Convolution2D(3, 32, ksize=3, pad=1)
+            self.c2 = L.Convolution2D(32, 64, ksize=3, pad=1)
+            self.c3 = L.Convolution2D(64, 64, ksize=3, pad=1)
+            self.fc1 = L.Linear(None, 128)
+            self.fc2 = L.Linear(None, n_out)
+            
+            self.bn1 = L.BatchNormalization(32)
+            self.bn2 = L.BatchNormalization(64)
+            self.bn3 = L.BatchNormalization(64)
+            
+    def __call__(self, x):
+        h = F.relu(self.bn1(self.c1(x)))
+        h = F.max_pooling_2d(h, ksize=2, stride=2)
+        h = F.relu(self.bn2(self.c2(h)))
+        h = F.max_pooling_2d(h, ksize=2, stride=2)
+        h = F.relu(self.bn3(self.c3(h)))
+        h = F.relu(self.fc1(h))
+        return self.fc2(h)
+    
+def gen_dataset(data):
+    image_rgb = data.copy()
+    image_bgr = data[:,::-1,:,:]
+    labels_rgb = np.zeros((len(data),), np.int32)
+    labels_bgr = np.ones((len(data),), np.int32)
+    images = np.concatenate((image_rgb, image_bgr), axis=0)
+    labels = np.concatenate((labels_rgb, labels_bgr), axis=0)
+
+    return chainer.datasets.tuple_dataset.TupleDataset(images, labels)
+
+def main():
+    parser = argparse.ArgumentParser(description='Chainer CIFAR example:')
+    parser.add_argument('--dataset', '-d', default='cifar10',
+                        help='The dataset to use: cifar10 or cifar100')
+    parser.add_argument('--batchsize', '-b', type=int, default=128,
+                        help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=50,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=0,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the training from snapshot')
+    args = parser.parse_args()
+
+    print('GPU: {}'.format(args.gpu))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# epoch: {}'.format(args.epoch))
+    print('')
+
+    # Set up a neural network to train.
+    # Classifier reports softmax cross entropy loss and accuracy at every
+    # iteration, which will be used by the PrintReport extension below.
+    if args.dataset == 'cifar10':
+        print('Using CIFAR10 dataset.')
+        class_labels = 10
+        train, test = get_cifar10(withlabel=False)
+    elif args.dataset == 'cifar100':
+        print('Using CIFAR100 dataset.')
+        class_labels = 100
+        train, test = get_cifar100(withlabel=False)
+    else:
+        raise RuntimeError('Invalid dataset choice.')
+        
+    train = gen_dataset(train)
+    test = gen_dataset(test)
+
+    model = L.Classifier(Net(2))
+    
+    if args.gpu >= 0:
+        # Make a specified GPU current
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()  # Copy the model to the GPU
+
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+                                                 repeat=False, shuffle=False)
+    # Set up a trainer
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+    # Evaluate the model with the test dataset for each epoch
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+
+    # Dump a computational graph from 'loss' variable at the first iteration
+    # The "main" refers to the target link of the "main" optimizer.
+    trainer.extend(extensions.dump_graph('main/loss'))
+
+    # Take a snapshot at each epoch
+    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
+
+    # Write a log of evaluation statistics for each epoch
+    trainer.extend(extensions.LogReport())
+
+    # Print selected entries of the log to stdout
+    # Here "main" refers to the target link of the "main" optimizer again, and
+    # "validation" refers to the default name of the Evaluator extension.
+    # Entries other than 'epoch' are reported by the Classifier link, called by
+    # either the updater or the evaluator.
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+
+    # Print a progress bar to stdout
+    trainer.extend(extensions.ProgressBar())
+
+    if args.resume:
+        # Resume from a snapshot
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    # Run the training
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
